1c863c4c0eaf5d505c11d0a76ccffdb048753f67
daw2-vegas26 <vegasromerojoel@fpllefia.com>
2026-05-05 16:10:11 +0200

    config

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,19 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global;
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // Obligamos a usar el motor directo, saltándonos cualquier detección de Proxy
-    engineType: 'binary', 
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
+// Creamos una configuración que obliga a Prisma a usar el motor de librería estándar
+const prismaClientOptions = {
+  log: ["query", "error", "warn"],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
     },
-  });
+  },
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient(prismaClientOptions);
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
