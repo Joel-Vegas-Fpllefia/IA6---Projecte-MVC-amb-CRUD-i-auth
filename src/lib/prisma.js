ab4537a061e86611e18a8aa74dbda1a7be7edd31
daw2-vegas26 <vegasromerojoel@fpllefia.com>
2026-05-05 16:26:05 +0200

    config

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -5,6 +5,8 @@ const globalForPrisma = global;
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    // Forzamos a Prisma a NO usar el motor de aceleración (Data Proxy)
+    // y a usar la URL de la variable de entorno directamente.
     datasources: {
       db: {
         url: process.env.DATABASE_URL,
